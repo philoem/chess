bb221f1261f2ee57c61d80e2dd57dd30961c09d3
Philoem <philoem24@gmail.com>
2023-06-02 20:09:51 +0200

    feat: add props to svg components3

--- src/components/GameBoard/index.tsx
+++ src/components/GameBoard/index.tsx
@@ -8,12 +8,12 @@ import Queen from '~/assets/pieces/Queen';
 import King from '~/assets/pieces/King';
 
 export default component$(() => {
-  const rook = <Rook />;
-  const pawn = <Pawn />;
-  const knight = <Knight />;
-  const bishop = <Bishop />;
-  const queen = <Queen />;
-  const king = <King />;
+  const rook = <Rook size={'2.3em'} color={'white'} />;
+  const pawn = <Pawn size={'2.3em'} color={'white'} />;
+  const knight = <Knight size={'2.3em'} color={'white'} />;
+  const bishop = <Bishop size={'2.3em'} color={'white'} />;
+  const queen = <Queen size={'2.3em'} color={'white'} />;
+  const king = <King size={'2.3em'} color={'white'} />;
 
   const allPieces = useStore([rook, knight, bishop, queen, king, bishop, knight, rook, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
     pawn, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 
